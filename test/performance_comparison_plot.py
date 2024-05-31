17da2a4b92d7d0996cf0f8278dfe9b8ac639637e
Matt <matt.anderson0987@gmail.com>
2024-05-31 01:55:51 -0400

    Add fastjet and boost performance measurements to plot

--- test/performance_comparison_plot.py
+++ test/performance_comparison_plot.py
@@ -4,16 +4,24 @@ import matplotlib.pyplot as plt
 
 
 JC_VORONOI_MEASUREMENTS_PATH = "../data/jc_voronoi_performance_measurements.json"
+FASTJET_MEASUREMENTS_PATH = "../data/fastjet_performance_measurements.json"
+BOOST_MEASUREMENTS_PATH = "../data/boost_performance_measurements.json"
 VORONOI_DIAGRAM_MEASUREMENTS_PATH = "../data/performance_measurements.json"
 
 def load_measurements():
     script_path = os.path.dirname(os.path.realpath(__file__))
     jc_path = os.path.join(script_path, JC_VORONOI_MEASUREMENTS_PATH)
+    fj_path = os.path.join(script_path, FASTJET_MEASUREMENTS_PATH)
+    b_path = os.path.join(script_path, BOOST_MEASUREMENTS_PATH)
     vd_path = os.path.join(script_path, VORONOI_DIAGRAM_MEASUREMENTS_PATH)
     jc_measurements = json.load(open(jc_path))
+    fj_measurements = json.load(open(fj_path))
+    b_measurements = json.load(open(b_path))
     vd_measurements = json.load(open(vd_path))
     return {
-        "jc_voronoi": jc_measurements["generate diagram"],
+        #"jc_voronoi": jc_measurements["generate diagram"],
+        "fastjet": fj_measurements["generate diagram"],
+        "boost (no clipping included)": b_measurements["generate diagram"],
         "VoronoiDiagram": vd_measurements["generate diagram"]
     }
 
