e8a35a867d0f885a6d6e770e8cec6d3cc9917739
Matt <matt.anderson0987@gmail.com>
2024-05-31 17:05:49 -0400

    change plot style to solarize light

--- test/performance_comparison_plot.py
+++ test/performance_comparison_plot.py
@@ -60,7 +60,7 @@ def plot(data, start, stop, tick_step=None):
 
 def main():
     data = load_measurements()
-    plt.style.use("fivethirtyeight")
+    plt.style.use("Solarize_Light2")
     plot(data, 0.0, 1000.0, 100.0)
     plot(data, 1000.0, 100000.0, 10000.0)
     #plot(data, 100000.0, 200000.0, 10000.0)
