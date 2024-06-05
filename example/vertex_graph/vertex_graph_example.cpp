45276a36a61ed5d0da70bfca031748997602c9e8
Matt <matt.anderson0987@gmail.com>
2024-06-04 20:48:26 -0400

    include the seeds used to generate diagram in the output

--- example/vertex_graph/vertex_graph_example.cpp
+++ example/vertex_graph/vertex_graph_example.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <vector>
 #include <utility>
@@ -7,18 +8,20 @@
 int main() {
     using namespace VoronoiDiagram;
     using namespace VoronoiDiagram::ExampleIO;
-    int seed = 1616;
     int nsites = 1000;
-    Bbox box{0.0, 4096.0, 0.0, 4096.0};
+    Bbox box{0.0, 0.00001, 0.0, 0.00001};
+    
+    int seed = 228241;
+    //int seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
     Calculator calculator(seed);
-
     calculator.generate(nsites, box);
-
     VertexGraph vgraph = calculator.get_vertex_graph();
     
     typedef std::vector<std::pair<RealCoordinate, RealCoordinate>> ls_vector;
     ls_vector vertex_line_segments = vertex_graph_to_line_segments(vgraph.get_vertices());
+    std::vector<RealCoordinate> seeds = calculator.get_seeds();
     std::cout << "Writing results" << std::endl;
     vector_output("vertex_line_segments.json", vertex_line_segments);
+    vector_output("diagram_seeds.json", seeds);
     std::cout << "Done" << std::endl;
 }
