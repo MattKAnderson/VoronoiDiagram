5d2a19b0dce6cdb594ed8edaeaed67878f69a23f
Matt <matt.anderson0987@gmail.com>
2024-06-04 20:49:13 -0400

    also plot seeds used to generate diagram

--- example/vertex_graph/visualize.py
+++ example/vertex_graph/visualize.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 xmin = 0.0
 ymin = 0.0
-xmax = 4096
-ymax = 4096
-padding = 100.0
+xmax = 0.00001
+ymax = 0.00001
+padding = 0.000001
 
 def plot_line_segments(ax, lines, color=None):
     for line in lines:
@@ -16,14 +16,21 @@ def plot_line_segments(ax, lines, color=None):
         else:
             ax.plot(xs, ys)
 
+def plot_seeds(ax, seeds, color="b"):
+    xs = [point[0] for point in seeds]
+    ys = [point[1] for point in seeds]
+    ax.scatter(xs, ys, color=color)
 
 base_dir = "build/example/vertex_graph/"
 f1 = open(base_dir + "vertex_line_segments.json")
+f2 = open(base_dir + "diagram_seeds.json")
 line_segments = json.load(f1)["vector"]
+seeds = json.load(f2)["vector"]
 
 fig, ax = plt.subplots(1)
 ax.set_xlim(xmin - padding, xmax + padding)
 ax.set_ylim(ymin - padding, ymax + padding)
 plot_line_segments(ax, line_segments, "r")
+plot_seeds(ax, seeds)
 plt.title("Voronoi Diagram produced from 1000 sites")
 plt.show()
